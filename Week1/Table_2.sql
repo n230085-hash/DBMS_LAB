7486d587dfcdcc5066560b1e978d3587d2ffe4ac
Siva kumar <n230085@rguktn.ac.in>
2026-07-22 22:37:53 +0530

    Adding a week1 folder

--- Week1/Table_2.sql
+++ Week1/Table_2.sql
@@ -8,7 +8,7 @@ CREATE TABLE Income_Category (
 );
 
 INSERT INTO Income_Category() VALUES
-									(1,'Salary','Income recieved from employment',TRUE),
+						(1,'Salary','Income recieved from employment',TRUE),
                                     (2,'Business','Income earned from business activities',TRUE),
                                     (3,'House Property','Income received from property or rent ',TRUE),
                                     (4,'Capital Gains', 'Income such as bank interst', TRUE),
@@ -16,3 +16,5 @@ INSERT INTO Income_Category() VALUES
                                     (6,'Agricultural Income','Income from eligible agricultural activities', TRUE);
 
 SELECT * FROM Income_Category;
+
+ALTER TABLE Income_Category RENAME COLUMN catagory_id TO category_id;
